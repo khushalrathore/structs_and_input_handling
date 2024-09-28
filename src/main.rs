f59b9ee2d9ae6e257d9ccab7a28632772de23db3
khushal <118611135+khushalrathore@users.noreply.github.com>
2024-09-28 21:38:46 +0530

    match statement

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,70 @@
-use std::io;
+use std::io::{self, Write};
+
 struct Rectangle {
     length: f32,
     breadth: f32,
 }
+
 impl Rectangle {
     fn new(length: f32, breadth: f32) -> Rectangle {
         Rectangle { length, breadth }
     }
+
     fn area(&self) -> f32 {
         self.length * self.breadth
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.breadth > other.breadth
+    }
 }
+
 fn main() {
-    print!("enter length & breadth (space-separated) : ");
-    io::Write::flush(&mut io::stdout()).expect("Flush Failed");
+    
+    print!("Enter length & breadth (space-separated): ");
+    io::stdout().flush().expect("Failed to flush stdout");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Invalid Input");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
     let inputs: Vec<f32> = input
         .trim()
         .split_whitespace()
-        .map(|s| s.parse().expect("enter valid number"))
+        .map(|s| s.parse().expect("Enter a valid number"))
         .collect();
+
     if inputs.len() != 2 {
         println!("Please enter exactly two numbers.");
         return;
     }
-    let (l, b) = (inputs[0], inputs[1]);
-    let r1 = Rectangle::new(l, b);
+
+    let (length, breadth) = (inputs[0], inputs[1]);
+    let r1 = Rectangle::new(length, breadth);
+    let r2 = Rectangle::new(1.0, 2.0);
+    let r3 = Rectangle::new(21.0, 22.0);
+
     println!(
         "Area of Rectangle({} x {}) is {}",
         r1.length,
         r1.breadth,
         r1.area()
     );
+
+    let can_hold_r2 = r1.can_hold(&r2);
+    let can_hold_r3 = r1.can_hold(&r3);
+
+    match (can_hold_r2, can_hold_r3) {
+        (true, true) => println!("Input rectangle can hold both rectangles."),
+        (true, false) => println!(
+            "Input rectangle can hold rectangle({} x {}).",
+            r2.length, r2.breadth
+        ),
+        (false, true) => println!(
+            "Input rectangle can hold rectangle({} x {}).",
+            r3.length, r3.breadth
+        ),
+        (false, false) => println!("Input rectangle can't hold either rectangle."),
+    }
 }
